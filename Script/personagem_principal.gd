59f13f2302c03c4f8892a8e3d86266a3dfb8ee11
fetrentin7 <fetrentin7@gmail.com>
2025-09-15 15:59:35 -0300

    Update personagem_principal.gd

--- Script/personagem_principal.gd
+++ Script/personagem_principal.gd
@@ -8,20 +8,19 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _physics_process(delta):
 
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		
 	else:
-		velocity.y = 0  
+		
 
 	# Pulo
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		
 
 	# Movimento horizontal
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 
 	move_and_slide()
